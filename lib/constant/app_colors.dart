1ad83da96338b4f1162e4ecfd67e2c5832a9f619
Mahdi_Mohammadi <jnsm89@yahoo.com>
2024-11-20 04:06:12 -0800

    add app color and set assets

---
+++ lib/constant/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+const primaryColor = Color(0xff0B8FAC);
+const secondaryColor = Color(0xff7BC1B7);
+const lightColor = Color(0xffD2EBE7);
+const lightBlue = Color(0xff8ED7F0);
+const blackColor = Color(0xff000000);
+const grayColor = Color(0xff858585);
+const whiteColor = Color(0xffffffff);
+const redColor = Color(0xffF30000);
+const orangeColor = Color(0xffF89603);
+const greenColor = Color(0xff129820);
+
